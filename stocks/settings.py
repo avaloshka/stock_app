bfe03b1311b9d9018ce8dc199f2915f3154634dc
Alex Valoshka <alexvaloshka@gmail.com>
2022-11-15 19:36:48 +0000

    changing key

--- stocks/settings.py
+++ stocks/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config("SECRET_KEY")
-SECRET_KEY = 'django-insecure-mhnrp9nd)u9c839k4^-$&p8gny$lmj5-a218k^))kcny2p@ti9'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
